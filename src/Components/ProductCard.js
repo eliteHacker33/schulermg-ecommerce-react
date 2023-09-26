efb57b89e664275424510f77b0070e6c7b278bef
eliteHacker33 <anambypamby@gmail.com>
2023-09-26 18:13:55 -0500

    cleaning up

--- src/Components/ProductCard.js
+++ src/Components/ProductCard.js
@@ -1,11 +1,11 @@
 import styles from "./ProductCard.module.css";
-import gun from "../../logo.svg";
+import gun from "../img/1970-BK-4593.jpg";
 
 const ProductCard = () => {
   return (
     <div className={styles["product-card-container"]}>
       <a href="#" className="goToGunDetails" />
-      <img src="../../public/1970-BK-4593.jpg"></img>
+      <img className={styles["product-image"]} src={gun}></img>
       <div className={styles["product-card-description"]}> fake gun</div>
       <div className={styles["product-price-information"]}>
         you wish you knew the price
